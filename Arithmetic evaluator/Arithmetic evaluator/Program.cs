2bad628ac89ea00ba00fb0a513a589e2421e2581
Jacob Huempfner <jacobrh94@hotmail.com>
2019-07-31 09:10:30 -0500

    Updates to Program.cs
    Added another test string. Also added ability to parse expressions passed in through a command-line argument. Changed assembly name to "ArithmeticParser".

--- Arithmetic evaluator/Arithmetic evaluator/Program.cs
+++ Arithmetic evaluator/Arithmetic evaluator/Program.cs
@@ -10,6 +10,13 @@ namespace Arithmetic_evaluator
     {
         static void Main(string[] args)
         {
+            // read function from command-line arguments if one was given
+            if (args.Length != 0)
+            {
+                Console.WriteLine(Parser.Evaluate(args[0]));
+                return;
+            }
+
             // test order of operations and negative numbers
             Console.WriteLine(Parser.Evaluate("(11-5)*2-3+1")); //  10
             Console.WriteLine(Parser.Evaluate("3+4*(2)")); //       11
@@ -17,12 +24,13 @@ namespace Arithmetic_evaluator
             Console.WriteLine(Parser.Evaluate("9-5/(8-3)*2+6")); // 13
             Console.WriteLine(Parser.Evaluate("-(3+4)*-2")); //     14
             Console.WriteLine(Parser.Evaluate("3+3 * 3+3")); //     15
+            Console.WriteLine(Parser.Evaluate("8 / 2(2+2)")); //    16
             Console.WriteLine(Parser.Evaluate("150/(6+3*8)-5")); // 0
             Console.WriteLine(Parser.Evaluate("(2-3)(4/6)")); //    -0.6666666...
 
             // test power operator ("^")
-            Console.WriteLine(Parser.Evaluate("5^2*2^4")); // 400
-            Console.WriteLine(Parser.Evaluate("4(0.4)^2")); // 0.64 or 64, depending upon culture
+            Console.WriteLine(Parser.Evaluate("5^2*2^4")); //       400
+            Console.WriteLine(Parser.Evaluate("4(0.4)^2")); //      0.64 or 64, depending upon culture
 
             try
             {
